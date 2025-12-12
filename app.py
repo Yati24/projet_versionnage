4a7463665932d83c1ee39cac97ce0ffd92a6dc24
Yati24 <cabayetom@yahoo.fr>
2025-12-12 17:08:11 +0100

    feat(home)création de la page d'accueil

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
